02efca1d15a0fd5c4a38540f74284a59e57c88fc
Kouki Badr <badreddinekouki1998@gmail.com>
2021-07-17 09:03:34 +0100

    create cherry toast layout

--- lib/cherry_toast.dart
+++ lib/cherry_toast.dart
@@ -3,8 +3,40 @@ import 'package:flutter/material.dart';
 class CherryToast extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          width: 250,
+          height: 50,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 2,
+                offset: Offset(0, 1), // changes position of shadow
+              ),
+            ],
+          ),
+        ),
+      ],
     );
+  }
+
+  show(BuildContext context) {
+    showDialog(
+        context: context,
+        barrierColor: Colors.white.withOpacity(0),
+        builder: (context) {
+          return AlertDialog(
+            backgroundColor: Colors.transparent,
+            contentPadding: EdgeInsets.all(0),
+            elevation: 0,
+            content: this,
+          );
+        });
   }
 }
