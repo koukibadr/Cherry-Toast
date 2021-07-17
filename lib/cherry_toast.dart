5cb471f09811882c6d98f029c1d752c28bf47417
Kouki Badr <badreddinekouki1998@gmail.com>
2021-07-17 08:19:54 +0100

    migrate project to null safety, create example project

--- lib/cherry_toast.dart
+++ lib/cherry_toast.dart
@@ -1,7 +1,10 @@
-library cherry_toast;
+import 'package:flutter/material.dart';
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class CherryToast extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
 }
