a86be6d41d31c4e5183acd92a85fe54c477f310b
Kouki Badr <badreddinekouki1998@gmail.com>
2021-07-19 11:09:46 +0100

    fix #5: add LTR and RTL layout parameter

--- lib/cherry_toast.dart
+++ lib/cherry_toast.dart
@@ -27,7 +27,8 @@ class CherryToast extends StatefulWidget {
       this.animationCurve = DEFAULT_ANIMATION_CURVE,
       this.animationType = ANIMATION_TYPE.FROM_LEFT,
       this.autoDismiss = false,
-      this.toastDuration = DEFAULT_TOAST_DURATION});
+      this.toastDuration = DEFAULT_TOAST_DURATION,
+      this.layout = TOAST_LAYOUT.LTR});
 
   CherryToast.success(
       {required this.title,
@@ -46,7 +47,8 @@ class CherryToast extends StatefulWidget {
       this.animationCurve = DEFAULT_ANIMATION_CURVE,
       this.animationType = ANIMATION_TYPE.FROM_LEFT,
       this.autoDismiss = false,
-      this.toastDuration = DEFAULT_TOAST_DURATION}) {
+      this.toastDuration = DEFAULT_TOAST_DURATION,
+      this.layout = TOAST_LAYOUT.LTR}) {
     this.icon = Image(
       image: AssetImage(SUCCESS_ICON, package: PACKAGE_NAME),
       width: 20,
@@ -70,7 +72,8 @@ class CherryToast extends StatefulWidget {
       this.animationCurve = DEFAULT_ANIMATION_CURVE,
       this.animationType = ANIMATION_TYPE.FROM_LEFT,
       this.autoDismiss = false,
-      this.toastDuration = DEFAULT_TOAST_DURATION}) {
+      this.toastDuration = DEFAULT_TOAST_DURATION,
+      this.layout = TOAST_LAYOUT.LTR}) {
     this.icon = Image(
       image: AssetImage(ERROR_ICON, package: PACKAGE_NAME),
       width: 20,
@@ -94,7 +97,8 @@ class CherryToast extends StatefulWidget {
       this.animationCurve = DEFAULT_ANIMATION_CURVE,
       this.animationType = ANIMATION_TYPE.FROM_LEFT,
       this.autoDismiss = false,
-      this.toastDuration = DEFAULT_TOAST_DURATION}) {
+      this.toastDuration = DEFAULT_TOAST_DURATION,
+      this.layout = TOAST_LAYOUT.LTR}) {
     this.icon = Image(
       image: AssetImage(WARNING_ICON, package: PACKAGE_NAME),
       width: 20,
@@ -118,7 +122,8 @@ class CherryToast extends StatefulWidget {
       this.animationCurve = DEFAULT_ANIMATION_CURVE,
       this.animationType = ANIMATION_TYPE.FROM_LEFT,
       this.autoDismiss = false,
-      this.toastDuration = DEFAULT_TOAST_DURATION}) {
+      this.toastDuration = DEFAULT_TOAST_DURATION,
+      this.layout = TOAST_LAYOUT.LTR}) {
     this.icon = Image(
       image: AssetImage(INFO_ICON, package: PACKAGE_NAME),
       width: 20,
@@ -142,6 +147,7 @@ class CherryToast extends StatefulWidget {
   final ANIMATION_TYPE animationType;
   final bool autoDismiss;
   final Duration toastDuration;
+  final TOAST_LAYOUT layout;
 
   show(BuildContext context) {
     showDialog(
@@ -216,6 +222,14 @@ class _CherryToastState extends State<CherryToast>
 
   @override
   Widget build(BuildContext context) {
+    if (this.widget.layout == TOAST_LAYOUT.LTR) {
+      return _renderLeftLayoutToast(context);
+    } else {
+      return _renderRightLayoutToast(context);
+    }
+  }
+
+  Column _renderLeftLayoutToast(BuildContext context) {
     return Column(
       mainAxisAlignment: this.widget.toastPosition == POSITION.TOP
           ? MainAxisAlignment.start
@@ -298,6 +312,100 @@ class _CherryToastState extends State<CherryToast>
                         image: AssetImage(CLOSE_ICON, package: PACKAGE_NAME),
                         width: 10,
                       ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Column _renderRightLayoutToast(BuildContext context) {
+    return Column(
+      mainAxisAlignment: this.widget.toastPosition == POSITION.TOP
+          ? MainAxisAlignment.start
+          : MainAxisAlignment.end,
+      children: [
+        SlideTransition(
+          position: offsetAnimation,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 1,
+                  blurRadius: 2,
+                  offset: Offset(0, 1), // changes position of shadow
+                ),
+              ],
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 10, left: 10),
+                    child: InkWell(
+                      onTap: () {
+                        slideController.reverse();
+                        Timer(this.widget.animationDuration, () {
+                          Navigator.pop(context);
+                        });
+                      },
+                      child: Image(
+                        image: AssetImage(CLOSE_ICON, package: PACKAGE_NAME),
+                        width: 10,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left: 10, right: 10),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.end,
+                              children: [
+                                this.widget.displayTitle
+                                    ? Text(this.widget.title,
+                                        style: this.widget.titleStyle)
+                                    : Container(),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                this.widget.description == null
+                                    ? Container()
+                                    : Text(this.widget.description ?? ""),
+                                SizedBox(
+                                  height: 5,
+                                ),
+                                this.widget.displayAction
+                                    ? InkWell(
+                                        onTap: () {
+                                          this.widget.actionHandler?.call();
+                                        },
+                                        child: Text(this.widget.action,
+                                            style: this.widget.actionStyle))
+                                    : Container()
+                              ],
+                            ),
+                          ),
+                        ),
+                        CherryToatIcon(
+                            color: this.widget.themeColor,
+                            icon: this.widget.icon),
+                      ],
                     ),
                   ),
                 ],
