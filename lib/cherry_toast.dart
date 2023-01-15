05472518f73fd165349e4667c81138360049c6b0
Kouki Badr <badreddinekouki1998@gmail.com>
2023-01-15 18:37:06 +0100

    Fix #30 : add postframecallback fix to main widget

--- lib/cherry_toast.dart
+++ lib/cherry_toast.dart
@@ -367,7 +367,17 @@ class _CherryToastState extends State<CherryToast>
         break;
       default:
     }
-    WidgetsBinding.instance.addPostFrameCallback((_) {
+
+    /// ! To support Flutter < 3.0.0
+    /// This allows a value of type T or T?
+    /// to be treated as a value of type T?.
+    ///
+    /// We use this so that APIs that have become
+    /// non-nullable can still be used with `!` and `?`
+    /// to support older versions of the API as well.
+    T? ambiguate<T>(T? value) => value;
+
+    ambiguate(WidgetsBinding.instance)?.addPostFrameCallback((_) {
       slideController.forward();
     });
   }
