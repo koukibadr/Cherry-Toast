2bda0b2f2c63d6d40e146f11c8e8f006eea23c5a
alihassan143 <alihassan143cool@gmail.com>
2023-02-20 15:00:15 +0500

    added background color so user can change this

--- lib/resources/colors.dart
+++ lib/resources/colors.dart
@@ -4,3 +4,4 @@ const Color errorColor = Color(0xffE43837);
 const Color successColor = Color(0xFF2F9449);
 const Color infoColor = Color(0xFF4E5CB9);
 const Color warningColor = Color(0xffFC9F00);
+const Color defaultBbackgroundColor = Color(0xFFFFFFFF);
